3168a36e57a111e431d04f0c9f9edadcfeb86d71
Sumayyah <sumayyah@ometria.com>
2020-10-26 17:42:29 +0000

    Gets lat, long coordinates for an address through Google's API

---
+++ Interacting_with_APIs/Geocoding_app.py
@@ -0,0 +1,19 @@
+import requests
+
+# example request form:
+# https://maps.googleapis.com/maps/api/geocode/outputFormat?parameters
+# The API will place the response in a `results` array
+
+
+def get_geocode(my_input):
+    key = "TO_BE_GIVEN"
+    BASE_URL = "https://maps.googleapis.com/maps/api/geocode/"
+    address = my_input.replace(' ', '%20')
+    address = address.replace(',', '%2C')
+    requests.get(f"{BASE_URL}json?address={address}&key={key}")
+    lat, long = results.json()[geometry][location]
+    print(f"\nTokyo, Japan is {lat}, {long}")
+
+
+coordinates = getGeocode("Tokyo, Japan")
+print(coordinates)
