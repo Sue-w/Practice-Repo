d855ac593acf985f655274745f8401de0e1c6817
Sumayyah <sumayyah@ometria.com>
2020-11-04 16:29:49 +0000

    testing

---
+++ highest_common_factor/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+random_numbers = [10, 144]
+inverse = random_numbers
+inverse.reverse()
+a, b = random_numbers
+a, b = b, a%b
+print(inverse)
+print(a)
+print(b)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
